65b217409e6a0f341304f878d6e042b8bc088749
Hao Qin <jim.nevergiveup@gmail.com>
2022-04-26 20:17:26 -0400

    Initial Commit

---
+++ Slots/SlotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotsApp.swift
+//  Slots
+//
+//  Created by Hao Qin on 4/26/22.
+//
+
+import SwiftUI
+
+@main
+struct SlotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
